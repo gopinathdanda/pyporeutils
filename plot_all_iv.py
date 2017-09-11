9eff90cee907cc81331b6f391ad0dc2a28dba75c
gopinathdanda <gopinath.danda@gmail.com>
2017-09-11 19:31:38 -0400

    Save plots recursively.

--- plot_all_iv.py
+++ plot_all_iv.py
@@ -6,7 +6,9 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 
-fname = "Data/IV/Chip PE/032717 Chip PE_LD4_1M_100mV_-100mV_03272017_162344.hkr"
+import os
+
+#fname = "Data/IV/Chip PE/IV/032717 Chip PE_LD4_1M_100mV_-100mV_03272017_162344.hkr"
 
 def csv_reader(file_obj):
     reader = csv.DictReader(file_obj, delimiter="\t")
@@ -20,18 +22,51 @@ def csv_reader(file_obj):
 def linear(m, b, x):
     return list(map(lambda y:m*y+b, x))
 
-with open(fname, 'r') as f_obj:
-    i, v = csv_reader(f_obj)
-    fig = plt.figure("IV")
-    ax = plt.subplot(111)
-    ax.scatter(v, i)
-    m, b = np.polyfit(v, i, 1)
-    type(m)
-    ax.plot(v, linear(m, b, v))
-    g = m*1e3   # Conductance in nS
-    print(g)
+def plot_iv(fname, save = True, raw_fname = "", dirname = ""):
+    with open(fname, 'r') as f_obj:
+        i, v = csv_reader(f_obj)
+        fig = plt.figure("IV")
+        ax = plt.subplot(111)
+        ax.scatter(v, i)
+        m, b = np.polyfit(v, i, 1)
+        type(m)
+        ax.plot(v, linear(m, b, v))
+        g = m*1e3   # Conductance in nS
+        print("G = %0.2f nS" % g)
     
-    ax.set_xlim([min(v)-50, max(v)+50])
-    ax.set_xlabel("Voltage (mV)", size = "large")
-    ax.set_ylabel("Ionic current (nA)", size = "large")
-    plt.show()
+        ax.set_xlim([min(v)-50, max(v)+50])
+        ax.set_xlabel("Voltage (mV)", size = "large")
+        ax.set_ylabel("Ionic current (nA)", size = "large")
+        ax.text(0.7, 0.1,("G = %0.2f nS" % (g)),
+             horizontalalignment='left',
+             verticalalignment='center',
+             transform = ax.transAxes)
+        if(save == True and raw_fname != "" and dirname != ""):
+            plt.savefig(dirname+"/IVs/"+raw_fname[:-4]+".png", dpi=300, bbox_inches = "tight")
+            plt.clf()
+        else:
+            plt.show()
+
+#plot_iv("Data/IV/Chip PD/032417 Chip PD_LD4_1M_100mV_-100mV_03242017_163304.hkr")      
+
+main_dirname = './Data/IV'
+
+for dirname, dirnames, filenames in os.walk(main_dirname):
+    
+    #print(dirnames)
+    #print(filenames)
+    
+    # print path to all subdirectories first.
+    if(dirname != './Data/IV'):
+        print("------------------")
+        print(dirname)
+        print("------------------")
+        for filename in filenames:
+            if(filename[-4:] == '.hkr'):
+                if(not os.path.isdir(dirname+"/IVs")):
+                    os.mkdir(dirname+"/IVs")
+                plot_iv(os.path.join(dirname, filename), save = True, raw_fname = filename, dirname = dirname)
+
+    # print path to all filenames.
+    #for filename in filenames:
+    #    print(os.path.join(dirname, filename))
