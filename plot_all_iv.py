ee0f7dcc21c8fb02068bc13c0b2e9f9c7d876cd6
gopinathdanda <gopinath.danda@gmail.com>
2017-09-12 11:00:43 -0400

    Add customization to plot_all_iv

--- plot_all_iv.py
+++ plot_all_iv.py
@@ -28,47 +28,58 @@ def avg_voltages(currents, voltages):
         u_current.append(np.mean(c[u_indices]))
     return([u_current, u_voltages])
 
-def plot_iv(fname, save = False, raw_fname = "", dirname = ""):
-    with open(fname, 'r') as f_obj:
-        raw_i, raw_v = csv_reader(f_obj)
-        i, v = avg_voltages(raw_i, raw_v)
+def plot_iv(fname, save = False, location = [], avg = True, fit = True, show_g = True):
+    with open(fname, 'rU') as f_obj:
+        i, v = csv_reader(f_obj)
+        
+        if(avg == True):
+            i, v = avg_voltages(i, v)
         
         fig = plt.figure("IV")
         ax = plt.subplot(111)
         ax.scatter(v, i)
         m, b = np.polyfit(v, i, 1)
         type(m)
-        ax.plot(v, linear(m, b, v))
+        if(fit == True):
+            ax.plot(v, linear(m, b, v))
         g = m*1e3   # Conductance in nS
         print("G = %0.2f nS" % g)
     
         ax.set_xlim([min(v)-50, max(v)+50])
         ax.set_xlabel("Voltage (mV)", size = "large")
         ax.set_ylabel("Ionic current (nA)", size = "large")
-        ax.text(0.7, 0.1,("G = %0.2f nS" % (g)),
+        if(show_g == True):
+            ax.text(0.7, 0.1,("G = %0.2f nS" % (g)),
              horizontalalignment='left',
              verticalalignment='center',
              transform = ax.transAxes)
-        if(save == True and raw_fname != "" and dirname != ""):
-            plt.savefig(dirname+"/IV_avg/"+raw_fname[-19:-4]+".png", dpi=300, bbox_inches = "tight")
+        if(save == True):
+            if(len(location) == 2): # If location is given
+                [dirname, img_folder] = location
+                final_fname = dirname+"/"+img_folder+"/"+fname[-19:-4]+".png"
+            else:   # Or else store in folder of file
+                final_fname = fname[:-4]+".png"
+            print(final_fname)
+            plt.savefig(final_fname, dpi=300, bbox_inches = "tight")
             plt.clf()
         else:
             plt.show()
 
-def recursive_plot(main_dirname):    
+def recursive_plot(main_dirname, img_folder):    
     for dirname, dirnames, filenames in os.walk(main_dirname):
-        if(dirname != './Data/IV'):
+        if(dirname != main_dirname):
             print("------------------")
             print(dirname)
             print("------------------")
             for filename in filenames:
                 if(filename[-4:] == '.hkr'):
-                    if(not os.path.isdir(dirname+"/IV_avg")):
-                        os.mkdir(dirname+"/IV_avg")
-                    plot_iv(os.path.join(dirname, filename), save = True, raw_fname = filename, dirname = dirname)
+                    if(not os.path.isdir(dirname+"/"+img_folder)):
+                        os.mkdir(dirname+"/"+img_folder)
+                    plot_iv(os.path.join(dirname, filename), save = True, location = [dirname, img_folder])
 
 
 
-#plot_iv("Data/IV/Chip_PK_LD3_1M_rewet01_800mV_-800mV_04262017_213147.hkr")
+plot_iv("Data/IV/073117 chip SD_LD3.5_1M_300mV_-300mV_07312017_145250.hkr", avg = True, fit = False, show_g = False)
 dirname = './Data/IV'
-recursive_plot(dirname)
+img_folder = "IV_avg"
+#recursive_plot(dirname, img_folder)
