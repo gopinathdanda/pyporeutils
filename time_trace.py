25b71c53bd4a1487658a958a8f846af1d5d11627
gopinathdanda <gopinath.danda@gmail.com>
2017-09-18 01:17:47 -0400

    Allow viewing of entire time trace by default.

--- time_trace.py
+++ time_trace.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
-# EXTRACT TIMES TRACES OF CURRENT OR CONDUCTANCE FROM HKD FILE
+'''
+Extract time traces of current or conductance from .hkd file
+'''
 
 import heka_reader as heka
 import numpy as np
 import matplotlib.pyplot as plt
 
-def time_trace(filename, start, stop, dec = True, dec_rate = 2500, conductance = False, vb = 0.1):
+def time_trace(filename, start = 0, stop = 0, dec = True, dec_rate = 2500, conductance = False, vb = 0.1):
     # Get file
     reader = heka.HekaReader(filename)
     all_data = reader.get_all_data(decimate = dec)
     data = all_data[0][0]
     sample_rate = reader.get_sample_rate()
+    if stop == 0:
+        stop = len(data)/sample_rate*1.0
     start_len = int(start*sample_rate)
     stop_len = int(stop*sample_rate)
     if dec:
@@ -28,7 +32,7 @@ def time_trace(filename, start, stop, dec = True, dec_rate = 2500, conductance =
     else:
         return np.asarray([i, t])
 
-g, t = time_trace('Data/ChipAU.hkd', 0, 250, conductance = True, vb = 0.1)
+g, t = time_trace('Data/ChipAU.hkd', dec = False)
 
 plt.plot(t, g, "black")
 #plt.ylim(0, 40)
